c161349dc742aec41504da55f39303e25efbd78e
Fran <nietofrancisco727@gmail.com>
2025-07-18 01:32:30 -0400

    Arreglando el token

--- controllers/authController.js
+++ controllers/authController.js
@@ -19,6 +19,8 @@ const createSendToken = (user, statusCode, res) => {
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
+    secure: process.env.NODE_ENV === 'development',
+    sameSite: 'None',
   }
   if (process.env.NODE_ENV === 'production') cookieOptions.secure = true
   res.cookie('jwt', token, cookieOptions)
